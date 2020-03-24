c4e45efc9bccfd32a427639d54700a4fc4e9442f
Aleksey-Golub <alekseigolub.ag@gmail.com>
2020-03-24 22:47:05 +0200

    v 0.1.1
    Добавить в бесконечный раннер:
    - +Стартовый экран
    - +убрать молот из рук
    - +счет в игре и вывод его на GameOverScreen(класс )
    - +надпись Жизни
    
    - картинка-фон для Финального экрана (картинка-фон)
    - дооформить кнопки
    - правильное отображение Орка и Мышей по слоям
    - фон игры: Трава на первом плане, Земля + трава на верхней кромке, Небо с облаками (типа паралакс, на самом деле просто медленнее движутся)
    - анимация Мыши (разрезать на три части): +полет, смерть(отскок и падение)
    - анимация Орк: +ходьба, бег(смесь в блед по мере роста скорости)
    - анимация смерти Орка (Орка шатается и падает, игра при этом продолжается, как только упадет -> все замирает через Time.timeScale + финальный экран)
    - ускорение с увеличением очков
    - ускорение по требованию (через Speed и Lerp ???)
    - меню паузы (через Time.timeScale)
    - добавить тени
    - выделить StartScreen в отдельную сцену и доделать Restart

---
+++ Бесконечный раннер/Assets/Scripts/UI/ScoreDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _scoreDisplay;
+
+    private float _elapsedTime = 0;
+    private int _score = 0;
+
+    private void Update()
+    {
+        if (Time.timeScale == 1)
+        {
+            _elapsedTime += Time.deltaTime;
+            _score = (int) _elapsedTime * 10;
+            _scoreDisplay.text = _score.ToString();
+        }
+    }
+}
